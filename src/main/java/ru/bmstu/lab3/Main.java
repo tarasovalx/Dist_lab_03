7344cbdfb12be5cb5d87bc44d20531e5d1fed4e5
tarasovalx <tarasovalx99@gmail.com>
2021-12-11 02:56:44 +0300

    Scripted auto-commit on change (2021-12-11 02:56:43) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/lab3/Main.java
@@ -0,0 +1,11 @@
+package ru.bmstu.lab3;
+
+import org.apache.spark.SparkConf;
+
+public class Main {
+    private static final String APP_NAME = "Lab_03";
+
+    public static void main(String[] args){
+
+    }
+}
