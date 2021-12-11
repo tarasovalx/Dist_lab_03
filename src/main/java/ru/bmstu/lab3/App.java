e66b498bca0332d48072fc5b75f3a65fd80f0cb7
tarasovalx <tarasovalx99@gmail.com>
2021-12-11 03:46:45 +0300

    Scripted auto-commit on change (2021-12-11 03:46:40) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/App.java
+++ src/main/java/ru/bmstu/lab3/App.java
@@ -1,15 +1,23 @@
 package ru.bmstu.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
-    private SparkConf conf;
+    private final SparkConf conf;
+    private static final  String OUTPUT_FILEPATH = "output";
+    private static final String AIRPORTS_DATA_FILENAME="airports.csv";
+    private static final String FLIGHTS_DATA_FILENAME="flights.csv";
 
     public App(SparkConf conf){
         this.conf = conf;
     }
 
     public void run(){
-
+        JavaSparkContext sc=new JavaSparkContext(conf);
+        JavaRDD<String> airports = sc.textFile(AIRPORTS_DATA_FILENAME);
+        JavaRDD<String> flights = sc.textFile(FLIGHTS_DATA_FILENAME);
+        flights.saveAsTextFile(OUTPUT_FILEPATH);
     }
 }
