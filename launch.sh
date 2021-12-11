ce87469ba8ec572628ea867861c6c3aabf602554
tarasovalx <tarasovalx99@gmail.com>
2021-12-11 03:31:10 +0300

    Scripted auto-commit on change (2021-12-11 03:31:09) by gitwatch.sh

--- launch.sh
+++ launch.sh
@@ -1,17 +1,13 @@
 mvn -X package
 
-start-dfs.sh
-start-yarn.sh
-
 hdfs dfs -rm -r output
 
 hadoop fs -copyFromLocal airports.csv
 hadoop fs -copyFromLocal flights.csv
 
-export HADOOP_CLASSPATH=./target/lab_03-1.0-SNAPSHOT.jar
 
-hadoop bmstu.ru.Main output
+rm -r output
 
-spark --class ru.bmstu.lab3.Main --master yarn-client --num-executors 3 target/spark-examples-1.0-SNAPSHOT.jar
+spark-submit --class ru.bmstu.lab3.Main --master yarn-client --num-executors 3 target/lab_03-1.0-SNAPSHOT.jar
 
 hadoop fs -copyToLocal output ~/labs_output/lab_03
